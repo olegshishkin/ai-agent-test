e203e52edbb4077c66bc034914ccddc3909af1f8
olegshishkin <olegshishkin@gmail.com>
2025-02-16 22:13:05 +0300

    simple ai agent

--- agent/nodes.py
+++ agent/nodes.py
@@ -10,36 +10,36 @@ SUMMARIZATION_NODE_NAME = "summarization"
 
 
 def classify(state: State):
-    ''' Классифицируй текст по одной из категорий: Новости, Блог, Исследования или Другое '''
+    """ Классифицируй текст по одной из категорий: Новости, Блог, Исследования или Другое """
 
     prompt = PromptTemplate(
             input_variables=["text"],
             template="Отнеси следующий текст к одной из категорий: Новости, Блог, Исследования или Другое.\n\Текст:{text}\n\Категория:"
     )
     message = HumanMessage(content=prompt.format(text=state["text"]))
-    kind = llm.predict_messages([message]).content.strip()
+    kind = llm.invoke([message]).content.strip()
     return {"kind": kind}
 
 
 def extract(state: State):
-    ''' Извлеки все объекты (Человек, Организация, Местоположение) из текста '''
+    """ Извлеки все объекты (Человек, Организация, Местоположение) из текста """
 
     prompt = PromptTemplate(
             input_variables=["text"],
             template="Извлеки все объекты (Человек, Организация, Местоположение) из следующего текста. Укажи результат в виде списка, разделенного запятыми. Перечисли только сами объекты, не указывай типы, не повторяй объекты\n\Текст:{text}\n\Объекты:"
     )
     message = HumanMessage(content=prompt.format(text=state["text"]))
-    entities = llm.predict_messages([message]).content.strip().split(", ")
+    entities = llm.invoke([message]).content.strip().split(", ")
     return {"entities": entities}
 
 
 def summarize(state: State):
-    ''' Резюме текста в одном коротком предложении '''
+    """ Резюме текста в одном коротком предложении """
 
     prompt = PromptTemplate(
             input_variables=["text"],
             template="Резюмируй следующий текст в одном коротком предложении.\n\Текст:{text}\n\Резюме:"
     )
     message = HumanMessage(content=prompt.format(text=state["text"]))
-    summary = llm.predict_messages([message]).content.strip()
+    summary = llm.invoke([message]).content.strip()
     return {"summary": summary}
